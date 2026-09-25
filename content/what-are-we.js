c8e5f72ab1e097baaf6bed6edc4c2103b6ee7be2
Seungyeon Chelsea Han <sh792@cornell.edu>
2026-09-25 10:46:57 -0400

    Refresh dating sim production link

--- content/what-are-we.js
+++ content/what-are-we.js
@@ -3,7 +3,7 @@ window.CHELSEA_CONTENT['what-are-we'] = {
     type: 'folder',
     title: 'what are we',
     icon: 'Assets/Images/folder_closed.ico',
-    launchUrl: 'content/experiences/datingsim/out/',
+    launchUrl: 'content/experiences/datingsim/out/?v=f5e28b0',
     documentation: ['A romance fantasy visual novel.'],
     links: []
 };
